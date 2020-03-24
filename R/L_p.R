2373bcd1024ca6210e45f4eabf04ac240eeb23a2
Kenneth Keuk <kenneth.keuk@gmail.com>
2020-03-24 11:40:09 +0900

    edits to be compatible with unidimensional point vectors and not only matrices
    Prototype for testing

--- R/L_p.R
+++ R/L_p.R
@@ -3,21 +3,21 @@
 # author : Facundo Mémoli
 # implementation of FLB (lk 467)
 # L_p(mu) part
-L_p <- function(X,Y,d_X,d_Y,mu_X,mu_Y, p = 1)
-{
+L_p <- function(X,Y,d_X,d_Y,mu_X,mu_Y, p = 1){
 
-      sXY <- s_XY(X,Y,d_X,d_Y,mu_X,mu_Y, p = p)
-      s_X <- sXY$s_X
-      s_Y <- sXY$s_Y
+   sXY <- s_XY(X,Y,d_X,d_Y,mu_X,mu_Y, p = p)
+   s_X <- sXY$s_X
+   s_Y <- sXY$s_Y
 
-      S <- matrix(NA,nrow = nrow(X), ncol = nrow(Y))
-      for(i in 1:nrow(X))
-      {
-            for(j in 1:nrow(Y))
-            {
-                  S[i,j] <- abs(s_X[i] - s_Y[j])
-            }
+   if(is.vector(X)) {n.X<- length(X)} else {n.X<- nrow(X)}
+   if(is.vector(Y)) {n.Y<- length(Y)} else {n.Y<- nrow(Y)}
+
+   S <- matrix(NA,nrow = n.X, ncol = n.X)
+   for(i in 1:n.X){
+      for(j in 1:n.X){
+         S[i,j] <- abs(s_X[i] - s_Y[j])
       }
-      S <- t(S)
-      return(list("obj_coef" = c(0.5 * S)))
+   }
+   S <- t(S)
+   return(list("obj_coef" = c(0.5 * S)))
 }
